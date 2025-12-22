029b859ed03be7e04aa8893edebb63ea7cfd909d
Satvik <satvikdronavalli@Satviks-MacBook-Pro-2.local>
2025-12-23 00:32:07 +0200

    improved invalid stroke filtering through relative shoulder movement detection

--- stroke_counter.py
+++ stroke_counter.py
@@ -1,5 +1,6 @@
 import mediapipe as mp
 import cv2
+import json
 
 '''
 TODO:
@@ -7,6 +8,9 @@ TODO:
 - Determine how to extract youtube videos
 
 '''
+def exponential_moving_average(cx,cy,px,py,a=0.3):
+    # From testing, a=0.3 minimized knee + body angle variance
+    return int(cx*a+px*(1-a)),int(cy*a+py*(1-a))
 
 
 def check_validity(path):
@@ -17,8 +21,8 @@ def check_validity(path):
     fps = cap.get(cv2.CAP_PROP_FPS)
     mpPose = mp.solutions.pose
     pose = mpPose.Pose(min_detection_confidence=0.85,min_tracking_confidence=0.85)
-    right_pose_list = [12,14,16,18,24,26,28]
-    left_pose_list = [11,13,15,17,23,25,27]
+    right_pose_list = [8,12,14,16,18,24,26,28]
+    left_pose_list = [7,11,13,15,17,23,25,27]
     pos_locations_dict = dict()
     left_visibility = None
     right_visibility = None
@@ -26,18 +30,25 @@ def check_validity(path):
     finish_frame = None
     catch = False
     catch_frame = None
-    shoulder = None
+    shoulder = 12
     waited = 0
     stroke_count = 0
     frame_idx = 0
     prev_stroke_rate = None
     temp = []
+    max_shoulder_pos = -1
+    min_shoulder_pos = 10e99
+    init_frame = 0
+    r_cx  = None
+    r_cy = None
+    r_px = None
+    r_py = None
     # Loop for video processing
 
     while True:
         if stroke_count >= 10:  # Heuristic, 10 contiguous strokes
             ten_good_strokes = temp
-            return True, ten_good_strokes
+            return ten_good_strokes
         success, img = cap.read()
         if not success or img is None:
             break # End of video
@@ -50,7 +61,8 @@ def check_validity(path):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = pose.process(imgRGB)
         if results.pose_landmarks: # If landmarks are detected
-
+            min_shoulder_pos = min(results.pose_landmarks.landmark[shoulder].x,min_shoulder_pos)
+            max_shoulder_pos = max(results.pose_landmarks.landmark[shoulder].x,max_shoulder_pos)
             # Side selection based on visibility
 
             if left_visibility is None and right_visibility is None:
@@ -70,7 +82,6 @@ def check_validity(path):
                             break
             visibilities = [results.pose_landmarks.landmark[lm].visibility for lm in curr_pose_list]
             if sum(visibilities) / len(curr_pose_list) < 0.85:
-                print("reset")
                 stroke_count = 0
                 pos_locations_dict = {}
                 finish = False
@@ -79,19 +90,22 @@ def check_validity(path):
                 continue
 
             # Determine stroke duration by logging catch and finish times
-
+            r_px = None
+            r_py = None
             for id, lm in enumerate(results.pose_landmarks.landmark):
-                h,w,c = img.shape
-                cx,cy = int(lm.x*w),int(lm.y*h)
+                h,w,_ = img.shape
+                r_cx,r_cy = lm.x,lm.y
+                if r_px and r_py:
+                    r_cx,r_cy = exponential_moving_average(r_cx,r_cy,r_px,r_py)
                 if id in curr_pose_list: # if the position is relevant
 
                     # Logs time that the finish is reached
 
                     if not finish:
                         if id not in pos_locations_dict.keys():
-                            pos_locations_dict.update({id: [(cx,cy)]})
+                            pos_locations_dict.update({id: [(r_cx,r_cy)]})
                         else:
-                            pos_locations_dict[id].append((cx,cy))
+                            pos_locations_dict[id].append((r_cx,r_cy))
                             # Right side
                             if id == 12 and len(pos_locations_dict[shoulder]) >= 7:
                                 # adjust this based on glitches
@@ -107,7 +121,7 @@ def check_validity(path):
                     # Logs time that the catch is reached (mirrored version of finish logic)
 
                     elif finish and not catch:
-                        pos_locations_dict[id].append((cx,cy))
+                        pos_locations_dict[id].append((r_cx,r_cy))
                         if waited < 10 and id == shoulder: # Accounts for directional change at the finish
                             waited += 1
                         else:
@@ -124,7 +138,8 @@ def check_validity(path):
                                     catch = True
         if catch and finish:
             stroke_rate = round(60*fps/(catch_frame-finish_frame),3)
-            if prev_stroke_rate is not None and abs(prev_stroke_rate-stroke_rate) <= 7:
+            shoulder_dist_traveled = round(max_shoulder_pos-min_shoulder_pos,3)
+            if prev_stroke_rate is not None and abs(prev_stroke_rate-stroke_rate) <= 7 and shoulder_dist_traveled > 0.25:
                 stroke_count += 1
                 temp.append(pos_locations_dict)
                 print(f"{stroke_count} contiguous {'stroke' if stroke_count == 1 else 'strokes'}")
@@ -133,8 +148,15 @@ def check_validity(path):
             finish = False
             waited = 0
             prev_stroke_rate = stroke_rate
+            max_shoulder_pos = -1
+            min_shoulder_pos = 10e99
+        cv2.imshow("Image", img)
         cv2.waitKey(1)
         frame_idx += 1
-    return False, []
+        r_px = r_cx
+        r_py = r_cy
+    return []
 
-print(check_validity("2000m Row in 7 Minutes Row Along  Real Time Tips.mp4"))
+val = check_validity("test_dir/test_vid_1")
+with open("ten_strokes.json", 'w') as output:
+    j_string = json.dump(val, output)
